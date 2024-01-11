5d4cb23bfcd5ebf588e80ed407db7dcd724e3a2d
Alena <alena.kaliuzhnaya@gmail.com>
2024-01-11 19:15:52 +0200

    init commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import altair as alt
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
