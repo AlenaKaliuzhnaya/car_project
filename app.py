2fe194bc8192f0cc7d73962405388aab848646e6
Alex <sashafish93@gmial.com>
2024-01-15 18:45:11 +0200

    changed percentage calc

--- app.py
+++ app.py
@@ -29,10 +29,11 @@ price_range = st.slider(
 
 
 filtered_data = data[(data['price'] >= price_range[0]) & (data['price'] <= price_range[1])]
+total_count_per_fuel = filtered_data['fuel'].value_counts()
 
-# car_models = st.multiselect(
-#     'Choose models of vehicle that you prefer',
-#     options=filtered_data['model'])
+# Calculate the percentage and create a new column 'count_percentage'
+filtered_data['count_percentage'] = (filtered_data.groupby('fuel')['fuel'].transform('count') / filtered_data['fuel']
+                                     .count()) * 100
 
 
 choose_new_car = st.checkbox('Show only new vehicles')
@@ -48,20 +49,10 @@ st.plotly_chart(fig, theme="streamlit")
 
 
 st.write('Distribution of vehicles by fuel type')
-filtered_data['price_percentage'] = (filtered_data.groupby('fuel')['price'].transform('sum') / filtered_data['price']
-                                     .sum()) * 100
+
 fig2 = px.bar(filtered_data, x="fuel", y="price_percentage", labels={"price_percentage": "Percent"})
 fig2.update_layout(yaxis_title="Percent", yaxis=dict(tickvals=list(range(0, 101, 10))))
 st.plotly_chart(fig2)
-
-st.write('Here are your options with a split by price, condition and model')
-
-fig3 = px.bar(
-   filtered_data, x="type", y="type",
-   labels={"type": "Count"},
-   title="Count of each Type"
-)
-st.plotly_chart(fig3)
 
 st.write('Here is the list of recommended vehicles')
 st.dataframe(filtered_data.sample(40))
