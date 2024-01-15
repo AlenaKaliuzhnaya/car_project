6c4154e3ec6d73327e6b3d8c30d9114325f85c58
Alex <sashafish93@gmial.com>
2024-01-15 17:55:53 +0200

    changed config

--- app.py
+++ app.py
@@ -51,12 +51,8 @@ st.write('Distribution of vehicles by fuel type')
 filtered_data['price_percentage'] = (filtered_data.groupby('fuel')['price'].transform('sum') / filtered_data['price']
                                      .sum()) * 100
 fig2 = px.bar(filtered_data, x="fuel", y="price_percentage", labels={"price_percentage": "Percent"})
-fig2.update_layout(yaxis=dict(
-    tickmode='array',
-    nticks=11,
-    tickvals=list(range(0, 101, 10)),  # Set tick values from 0 to 100 with a step of 10
-    ticktext=[f"{i}%" for i in range(0, 101, 10)]  # Format tick text as percent values
-))
+fig2.update_yaxes(tickvals=list(range(0, 101, 10)), ticktext=[f"{i}%" for i in range(0, 101, 10)])
+
 st.plotly_chart(fig2)
 
 st.write('Here are your options with a split by price, condition and model')
