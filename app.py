690cba019e15e91a8d56a698c857aa1f8503dbb3
Alena <alena.kaliuzhnaya@gmail.com>
2024-01-15 12:29:15 +0200

    fixed issue

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 import altair as alt
 
-data = pd.read_csv('vehicles_us.csv').drop_duplicates()
+data = pd.read_csv('vehicles_us.csv')
 
 st.header('Choose your car!')
 st.subheader('Use this app to select vehicle based on your preferences ')
@@ -23,32 +23,29 @@ st.caption(':red[Choose your parameters here]')
 
 price_range = st.slider(
     "What is your price range?",
-    value=(1.000000, 375000.000000))
+    min_value=1.000000,
+    max_value=375000.000000,
+    value=(1.000000, 375000.000000)
+)
 
-# actual_range=list(range(price_range[0],price_range[1]+1))
 actual_range = list(range(int(price_range[0]), int(price_range[1]) + 1))
 
-options = data['model']
-
 car_models = st.multiselect(
-    'Choose models of vehicle that you prefer',
-    options=options[len(options)])
+    'Choose models of vehicles that you prefer',
+    options=data['model']
+)
 
 choose_new_car = st.checkbox('Show only new vehicles')
 
 if choose_new_car:
-    filtered_data = data[data.price.isin(actual_range)]
-    filtered_data = filtered_data[data.condition == 'new']
-    filtered_data = filtered_data[data.car_models.isin()]
+    filtered_data = data[(data['price'].isin(actual_range)) & (data['condition'] == 'new') & (data['model'].isin(car_models))]
 else:
-    filtered_data = data[data.price.isin(actual_range)]
+    filtered_data = data[data['price'].isin(actual_range)]
 
-st.write('Here are your options with a split by price, condition  and model of the vehicle')
+st.write('Here are your options with a split by price, condition, and model of the vehicle')
 
 fig = px.scatter(filtered_data, x="model", y="price", color="condition", hover_name="model", log_x=True, size_max=60)
 st.plotly_chart(fig)
-
-# size="pop",
 
 st.write('Distribution of vehicles by fuel type')
 fig2 = px.histogram(filtered_data, x="fuel", y="price")
