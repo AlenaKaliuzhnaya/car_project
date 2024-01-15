ff0072659834d55164c4c758241a21845235954e
Alena <alena.kaliuzhnaya@gmail.com>
2024-01-15 11:52:27 +0200

    reworked logic

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import altair as alt
+
+data = pd.read_csv('vehicles_us.csv')
+
+st.header('Choose your car!')
+st.subheader('Use this app to select vehicle based on your preferences ')
+
 import urllib.request
 from PIL import Image
 
-data = pd.read_csv('vehicles_us.csv')
-st.header('Choose your car!')
-st.subheader('Use this app to select vehicle based on your preferences ')
 urllib.request.urlretrieve(
     'https://machineswithsouls.com/wp-content/uploads/2021/06/bmw-m-cars.jpg',
     "machineswithsouls.jpg")
@@ -27,31 +30,23 @@ actual_range = list(range(int(price_range[0]), int(price_range[1]) + 1))
 
 car_models = st.multiselect(
     'Choose models of vehicle that you prefer',
-    [data['model']])
-car_models = st.multiselect(
-    'Choose models of vehicle that you prefer',
-    options=['model'])
-car_models = st.multiselect(
-    'Choose models of vehicle that you prefer',
-    options=[str(model) for model in ['model']]
-)
-
-st.write('You selected:', car_models)
+    options=data['model'])
 
 choose_new_car = st.checkbox('Show only new vehicles')
 
 if choose_new_car:
     filtered_data = data[data.price.isin(actual_range)]
     filtered_data = filtered_data[data.condition == 'new']
-    filtered_data = filtered_data[data.car_models]
+    filtered_data = filtered_data[data.car_models.isin()]
 else:
     filtered_data = data[data.price.isin(actual_range)]
 
 st.write('Here are your options with a split by price, condition  and model of the vehicle')
 
-fig = px.scatter(filtered_data, x="model", y="price", size="pop", color="condition", hover_name="model", log_x=True,
-                 size_max=60)
+fig = px.scatter(filtered_data, x="model", y="price", color="condition", hover_name="model", log_x=True, size_max=60)
 st.plotly_chart(fig)
+
+# size="pop",
 
 st.write('Distribution of vehicles by fuel type')
 fig2 = px.histogram(filtered_data, x="fuel", y="price")
