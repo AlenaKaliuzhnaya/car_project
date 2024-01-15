2b894126bf6f6866819f3ebac1581ac464bad948
Alex <sashafish93@gmial.com>
2024-01-15 18:50:10 +0200

    Changed albel

--- app.py
+++ app.py
@@ -29,11 +29,7 @@ price_range = st.slider(
 
 
 filtered_data = data[(data['price'] >= price_range[0]) & (data['price'] <= price_range[1])]
-total_count_per_fuel = filtered_data['fuel'].value_counts()
 
-# Calculate the percentage and create a new column 'count_percentage'
-filtered_data['count_percentage'] = (filtered_data.groupby('fuel')['fuel'].transform('count') / filtered_data['fuel']
-                                     .count()) * 100
 
 
 choose_new_car = st.checkbox('Show only new vehicles')
@@ -50,8 +46,12 @@ st.plotly_chart(fig, theme="streamlit")
 
 st.write('Distribution of vehicles by fuel type')
 
-fig2 = px.bar(filtered_data, x="fuel", y="price_percentage", labels={"price_percentage": "Percent"})
-fig2.update_layout(yaxis_title="Percent", yaxis=dict(tickvals=list(range(0, 101, 10))))
+total_count_per_fuel = filtered_data['fuel'].value_counts()
+
+filtered_data['count_percentage'] = (filtered_data.groupby('fuel')['fuel'].transform('count') / filtered_data['fuel']
+                                     .count()) * 100
+
+fig2 = px.bar(filtered_data, x="fuel", y="count_percentage", labels={"count_percentage": "Percent"})
 st.plotly_chart(fig2)
 
 st.write('Here is the list of recommended vehicles')
